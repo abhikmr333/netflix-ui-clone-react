9e1fb8747280c106442b08805dbea1d50d0d7e33
abhikmr333 <abhi363642@gmail.com>
2024-09-09 20:39:36 +0530

    chore(setup): install and configure tailwindcss

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
+import "./style.css";
 
 const rootNode = document.getElementById("root");
 const root = createRoot(rootNode);
-root.render(<h1> hello </h1>);
+root.render(<h1 className="text-red-700"> hello </h1>);
