92bdf4f599373890848239456a18cf00f92c2b68
abhikmr333 <abhi363642@gmail.com>
2024-09-11 11:38:43 +0530

    style(Header): add black gradient from top to bottom

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import { NETFLIX_LOGO } from "../utils/constants";
 
 const Header = () => {
   return (
-    <header>
+    <header className="bg-gradient-to-b from-black">
       <img className="h-20 w-48" src={NETFLIX_LOGO} alt="Netflix logo" />
     </header>
   );
