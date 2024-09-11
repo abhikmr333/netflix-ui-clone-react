9e7f17cd82eac197c922b2668f6c2cc5f6acb0c6
abhikmr333 <abhi363642@gmail.com>
2024-09-11 07:47:35 +0530

    style(Header): style app logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
 const Header = () => {
-    return (
-        <header>
-            <h1> Netflix </h1>
-        </header>
-    );
+  return (
+    <header>
+      <h1 className="pl-12 pt-4 text-5xl font-bold text-[#e50914]">Netflix</h1>
+    </header>
+  );
 };
 
 export default Header;
