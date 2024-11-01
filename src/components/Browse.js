bc1679e0e16fba770247a3f956ccbdfae3e7c4b3
abhikmr333 <abhi363642@gmail.com>
2024-11-01 14:05:57 +0530

    refactor(hooks): convert movies API fetching logic to custom hook

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,27 +1,7 @@
-import { useEffect } from "react";
-import { MOVIES_URL } from "../utils/constants";
-import API_OPTIONS from "../utils/tmdbOption";
-import { useDispatch } from "react-redux";
-import { addMovies } from "../redux/features/movieSlice";
+import useNowPlayingMovies from "../utils/hooks/useNowPlayingMovies";
 
 const Browse = () => {
-  useEffect(() => {
-    getNowPlayingMovies();
-  }, []);
-
-  const dispatch = useDispatch();
-
-  const getNowPlayingMovies = async () => {
-    try {
-      const response = await fetch(MOVIES_URL, API_OPTIONS);
-      if (!response.ok) throw new Error("Error in fetching");
-      const result = await response.json();
-      console.log(result);
-      dispatch(addMovies(result.results));
-    } catch (err) {
-      console.log(err.message);
-    }
-  };
+  useNowPlayingMovies();
 };
 
 export default Browse;
---
+++ src/utils/hooks/useNowPlayingMovies.js
@@ -0,0 +1,27 @@
+import { useEffect } from "react";
+import { MOVIES_URL } from "../constants";
+import API_OPTIONS from "../tmdbOption";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../redux/features/movieSlice";
+
+const useNowPlayingMovies = () => {
+  useEffect(() => {
+    getNowPlayingMovies();
+  }, []);
+
+  const dispatch = useDispatch();
+
+  const getNowPlayingMovies = async () => {
+    try {
+      const response = await fetch(MOVIES_URL, API_OPTIONS);
+      if (!response.ok) throw new Error("Error in fetching");
+      const result = await response.json();
+      console.log(result);
+      dispatch(addMovies(result.results));
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+};
+
+export default useNowPlayingMovies;
