4e3eab2d554e1072b35efa90362cb5d475cdb99b
abhikmr333 <abhi363642@gmail.com>
2024-11-01 17:57:58 +0530

    feat(MovieDetail): create MovieDetail component which renders the detail of movie on main section

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -2,6 +2,8 @@ import useNowPlayingMovies from "../utils/hooks/useNowPlayingMovies";
 
 const Browse = () => {
   useNowPlayingMovies();
+
+  //Main section and Secondary section
 };
 
 export default Browse;
