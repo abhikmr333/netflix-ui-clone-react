0995cbfc6a3d7406ccce1350c55af6ce1b50ae4c
abhikmr333 <abhi363642@gmail.com>
2024-11-02 18:11:09 +0530

    feat(store): add new property trailerKey to initial state and a new action addTrailerKey

--- src/components/MovieVideo.js
+++ src/components/MovieVideo.js
@@ -1,6 +1,8 @@
 import { VIDEO_URL } from "../utils/constants";
 import { useEffect } from "react";
 import API_OPTIONS from "../utils/tmdbOption";
+import { addTrailerKey } from "../redux/features/movieSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 const MovieVideo = ({ movieId }) => {
   //early return if movieId is null
@@ -8,6 +10,8 @@ const MovieVideo = ({ movieId }) => {
   useEffect(() => {
     getMovieTrailer();
   }, []);
+  const dispatch = useDispatch();
+  const trailerKey = useSelector((store) => store?.movies?.trailerKey);
 
   const getMovieTrailer = async () => {
     try {
@@ -17,22 +21,27 @@ const MovieVideo = ({ movieId }) => {
       );
       if (!response.ok) throw new Error("fetching failed");
       const result = await response.json();
-      console.log(result);
+      const movieTrailerKey = filterMovieTrailer(result?.results);
+      dispatch(addTrailerKey(movieTrailerKey));
     } catch (err) {
       console.log(err.message);
     }
   };
 
+  const filterMovieTrailer = (result) => {
+    const movieTrailer = result.filter((res) => res?.type === "Trailer");
+    return movieTrailer.length ? movieTrailer[0]?.key : result[0]?.key;
+  };
+  console.log(trailerKey);
   return (
     <iframe
       className="aspect-video w-screen"
-      src="https://www.youtube.com/embed/tf9vUQgg1b4?si=mveute1omyu7PQCC&autoplay=1&mute=1&vq=hd1080p"
+      src={`https://www.youtube.com/embed/${trailerKey}?autoplay=1&mute=1&vq=hd1080p&loop=1&playlist=${trailerKey}`}
       title="YouTube video player"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowFullScreen
     ></iframe>
   );
 };
 
 export default MovieVideo;
-
-//movie_id/videos?language=en-US
--- src/redux/features/movieSlice.js
+++ src/redux/features/movieSlice.js
@@ -4,13 +4,17 @@ const moviesSlice = createSlice({
   name: "movies",
   initialState: {
     nowPlayingMovies: null,
+    trailerKey: null,
   },
   reducers: {
     addMovies: (state, action) => {
       state.nowPlayingMovies = action.payload;
     },
+    addTrailerKey: (state, action) => {
+      state.trailerKey = action.payload;
+    },
   },
 });
 
 export const movieReducer = moviesSlice.reducer;
-export const { addMovies } = moviesSlice.actions;
+export const { addMovies, addTrailerKey } = moviesSlice.actions;
