2eeb47e2714e3ffea50c75423873e3d772f4d631
abhikmr333 <abhi363642@gmail.com>
2024-12-27 17:45:52 +0530

    feat: movie suggestion search functionality using gemini API

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,35 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { geminiApiKey } from "../utils/geminiApiKey";
+import { useRef } from "react";
+
 const SearchBar = () => {
+  const searchQuery = useRef(null);
+
+  const searchPrompt = () => {
+    const { value } = searchQuery.current;
+    suggestMovies(value);
+  };
+  const suggestMovies = async (query) => {
+    try {
+      const genAI = new GoogleGenerativeAI(geminiApiKey);
+      const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+      const prompt = `Recommend me 5 ${query} movies only names separated by commas(act as a movie recommendation system)`;
+      const result = await model.generateContent(prompt);
+      console.log(result.response.text());
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
   return (
     <div className="pt-32">
       <form className="flex px-28" onSubmit={(e) => e.preventDefault()}>
-        <input className="w-[75%] border-2" type="text" />
-        <button className="ml-2 w-[25%] rounded-md bg-red-600 p-2 text-white">
+        <input ref={searchQuery} className="w-[75%] border-2" type="text" />
+        <button
+          onClick={searchPrompt}
+          className="ml-2 w-[25%] rounded-md bg-red-600 p-2 text-white"
+        >
           Search
         </button>
       </form>
---
+++ src/utils/geminiApiKey.example.js
@@ -0,0 +1,1 @@
+export const geminiApiKey = "YOUR KEY";
